23c103331e5a6996272257715db2f264c98bc2dc
mattellis91 <mathew.ellis2@griffithuni.edu.au>
2023-11-22 12:42:24 +1000

    added basic parsing for let statements

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/mattellis91/zima/token"
+
 type Node interface {
 	TokenLiteral() string
 }
@@ -15,7 +17,7 @@ type Expression interface {
 }
 
 type Program struct {
-	Statements	[]Statement
+	Statements []Statement
 }
 
 func (p *Program) TokenLiteral() string {
@@ -25,3 +27,20 @@ func (p *Program) TokenLiteral() string {
 		return ""
 	}
 }
+
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
